ae4708f8f139d58f4785e18b11562916ed9651fd
Milly Palmer <palmermilly9@gmail.com>
2026-02-09 08:32:39 +0000

    Introduce game-over function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+from turtle import update
 import pygame
-# import time
+import time
 import random
 
 snake_speed = 15
@@ -67,10 +68,45 @@ color)
     # displaying text
     game_window.blit(score_surface, score_rect)
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    # game over function
+    def game_over():
 
-pygame.quit()
+        # creating font object my_font
+        my_font = pygame.font.SysFont('times new roman', 50)
+
+        # creating a text surface on which text
+        # will be drawn
+        game_over_surface = my_font.render('Your Score is : ' + str(score), True, red)
+
+        # create a rectangular object for the text
+        # surface object
+        game_over_rect = game_over_surface.get.rect()
+
+        # setting position of the text
+        game_over_rect.midtop = (window_x/2, window_y/4)
+
+        # blit will draw the text on screen
+        game_window.blit(game_over_surface, game_over_rect)
+        pygame.display.flip()
+
+        # after 2 seconds we will quit the
+        # program
+        time.sleep(2)
+
+        running = True
+        clock = pygame.time.Clock()
+
+        while running:
+         for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+            running = False
+        
+           update()
+           pygame.display.update()
+           clock.tick(60)
+
+        # deactivating pygame library
+        pygame.quit()
+
+        # quit the program
+        # quit()
